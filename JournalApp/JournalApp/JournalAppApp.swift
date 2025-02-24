11cb277349c47fb263bb02750c6c15ed90a2055e
sarahdadoun01 <85398444+sarahdadoun@users.noreply.github.com>
2025-02-24 10:46:51 -0500

    added empty xcode project

---
+++ JournalApp/JournalApp/JournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppApp.swift
+//  JournalApp
+//
+//  Created by Sarah Dadoun on 2025-02-24.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
