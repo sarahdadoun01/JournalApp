51c3016be0828a52bdacb2baf910480032b6f1de
sarahdadoun01 <85398444+sarahdadoun@users.noreply.github.com>
2025-02-25 12:02:48 -0500

    added package dependencies

--- JournalApp/JournalApp/JournalAppApp.swift
+++ JournalApp/JournalApp/JournalAppApp.swift
@@ -5,13 +5,32 @@
 //  Created by Sarah Dadoun on 2025-02-24.
 //
 
+//import SwiftUI
+//
+//@main
+//struct JournalAppApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
+
+
 import SwiftUI
+import FirebaseCore
 
 @main
 struct JournalAppApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+
